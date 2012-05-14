ba6cf41b3655628973e92a7c3f2750f066481341
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-05-15 02:04:47 +0930

    Stub out controller mixing.

--- lib/islay/controller.rb
+++ lib/islay/controller.rb
@@ -1,5 +1,24 @@
 module Islay
   module AdminController
+    def self.included(klass)
+      klass.class_eval do
+        include InstanceMethods
+        extend ClassMethods
 
+        # Add helpers
+        # Install auth
+        # Add any before_filters
+
+        layout 'layouts/islay/application'
+      end
+    end
+
+    module InstanceMethods
+
+    end
+
+    module ClassMethods
+
+    end
   end
 end
