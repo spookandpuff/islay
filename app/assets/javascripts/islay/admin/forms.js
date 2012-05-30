f2cc5c5a51b31eb4cfa44b81000e2129ea8a1fd2
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-05-30 12:08:00 +0930

    First cut of working select and boolean widgets.

--- app/assets/javascripts/islay/admin/forms.js
+++ app/assets/javascripts/islay/admin/forms.js
@@ -26,7 +26,7 @@ Islay.Form = Backbone.View.extend({
   },
 
   initializeWidgets: function(obj, el) {
-    var $input = $(el).find(':input');
+    var $input = $(el).find('[type!=hidden]:input');
     var widget = null;
 
     // TODO: Account for groups like radio and checkboxes
@@ -34,34 +34,24 @@ Islay.Form = Backbone.View.extend({
     switch($input.prop('nodeName')) {
       case 'SELECT':
         widget = 'Select'
-        // Could be one of
-        // * select
-        // * multi-select
-        // * tags*
-      break;
-      case 'TEXTAREA':
-        widget = 'Textarea'
       break;
       case 'INPUT':
         switch($input.attr('type')) {
-          case 'file':
-            widget = 'File'
-          break;
-          default:
-            widget = 'Text'
+          case 'checkbox':
+            widget = 'Boolean'
           break;
         }
       break;
     }
 
-    var instance = new Islay.Widgets[widget]({el: el, input: $input})
+    if (widget) {
+      var instance = new Islay.Widgets[widget]({el: el, input: $input})
+      obj[$input.attr('id')] = instance;
+      instance.render();
 
-    obj[$input.attr('id')] = instance;
+      // TODO: Bind widgets to model
 
-    $input.after(instance.render().el);
-    $input.remove();
-
-    // TODO: Bind widgets to model
+    }
 
     return obj;
   },
@@ -74,41 +64,147 @@ Islay.Form = Backbone.View.extend({
 Islay.Widgets = Islay.Widgets || {};
 
 Islay.Widgets.Base = Backbone.View.extend({
-  events: {click: 'click', keyup: 'keyup'},
+  events: {'click': 'click', keyup: 'keyup'},
 
   initialize: function() {
     _.bindAll(this, 'click', 'keyup');
 
-    var name = this.options.input.attr('name');
-    this.$hiddenEl = $('<input type="hidden" name"' + name + '"/>');
+    this.name = this.options.input.attr('name');
+    this.$hiddenEl = $('<input type="hidden" name="' + this.name + '"/>');
+    this.$hiddenEl.val(this.options.input.val());
     this.$el.append(this.$hiddenEl);
+    this.options.input.remove();
+    this.data = this.extractData();
+  },
+
+  update: function(value) {
+    this.trigger('update', this.name, value);
+    this.$hiddenEl.val(value);
+  },
+
+  extractData: function() {
+    return null;
   },
 
   render: function() {
-    throw "Unimplimented";
+    throw "Unimplemented";
   },
 
   click: function() {
-    throw "Unimplimented";
+    throw "Unimplemented";
   },
 
   keyup: function() {
-    throw "Unimplimented";
+    throw "Unimplemented";
   },
 
   value: function() {
     this.$hiddenEl.val();
+  }
+});
+
+/* -------------------------------------------------------------------------- */
+/* SIMPLE SELECT
+/* -------------------------------------------------------------------------- */
+Islay.Widgets.Select = Islay.Widgets.Base.extend({
+  extractData: function() {
+    return _.map(this.options.input.find('option'), function(o) {
+      var $o = $(o);
+      return {text: $o.text(), value: $o.attr('value')};
+    });
   },
 
-  change: function() {
-    this.trigger('change', this.value())
-  }
-});
+  open: function() {
+    this.isOpen = true;
+    this.widget.addClass('open');
+    this.list.show();
+  },
 
-Islay.Widgets.Select = Islay.Widgets.Base.extend({
+  close: function() {
+    this.isOpen = false;
+    this.widget.removeClass('open');
+    this.list.hide();
+  },
+
+  click: function(e) {
+    var target = $(e.target);
+
+    if (target.is('.frame, .button, .display, span')) {
+      if (this.isOpen) {
+        this.close();
+      }
+      else {
+        this.open();
+      }
+    }
+    else if (target.is('li')) {
+      this.display.text(target.text());
+      this.update(target.attr('data-value'));
+      this.close();
+    }
+  },
+
   render: function() {
+    this.display = $H('div.display');
+    this.button = $H('div.button', $H('span', '>'));
+    var frame = $H('div.frame', [this.display, this.button]);
+    this.list = $H('ul.list');
 
+    this.widget = $H('div.widget.select', [frame, this.list]);
+
+    var currentVal = this.value();
+    _.each(this.data, function(entry) {
+      if (entry.value === currentVal) {
+        this.display = entry.text;
+      }
+      if (entry.value !== '') {
+        this.list.append($H('li', {'data-value': entry.value}, entry.text));
+      }
+    }, this);
+
+    this.$el.append(this.widget);
+
+    return this;
   }
 });
 
+/* -------------------------------------------------------------------------- */
+/* BOOLEAN SWITCH
+/* -------------------------------------------------------------------------- */
+Islay.Widgets.Boolean = Islay.Widgets.Base.extend({
+  click: function(e) {
+    var target = $(e.target);
 
+    if (target.is('li, span')) {
+      if (this.$hiddenEl.val() == 1) {
+        this.update(0);
+        this.optionOn.removeClass('selected');
+        this.optionOff.addClass('selected');
+      }
+      else {
+        this.update(1);
+        this.optionOff.removeClass('selected');
+        this.optionOn.addClass('selected');
+      }
+    }
+  },
+
+  render: function() {
+    this.optionOff = $H('li.button.optionOff', $H('span', 'x'));
+    this.optionOn = $H('li.button.optionOn', $H('span', 't'));
+    var frame = $H('ul.frame', [this.optionOff, this.optionOn]);
+    this.widget = $H('div.widget.boolean', frame);
+    this.$el.append(this.widget);
+
+    // Set initial state
+    this.$hiddenEl.val(this.options.input.checked);
+    if (this.options.input.checked) {
+      this.optionOn.addClass('selected');
+    }
+    else {
+      this.optionOff.addClass('selected');
+    }
+
+    return this;
+  }
+});
--- app/assets/javascripts/islay/application.js
+++ app/assets/javascripts/islay/application.js
@@ -2,3 +2,9 @@
 //= require ../vendor/underscore
 //= require ../vendor/backbone
 //= require_tree .
+
+$SP.where('.[edit, new]').select('#islay-form').run(function(form) {
+  var FormView = new Islay.Form({el: form});
+});
+
+$($SP.init);
