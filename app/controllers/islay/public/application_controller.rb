3921969bafe2b08c15673ef0d4b4c4c1cd027a3f
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-06-20 16:47:24 +0930

    Stub out basic public layout and #path helper.

--- app/controllers/islay/public/application_controller.rb
+++ app/controllers/islay/public/application_controller.rb
@@ -1,3 +1,22 @@
 class Islay::Public::ApplicationController < ActionController::Base
+  layout 'islay/public'
 
+  helper_method :path
+
+  private
+
+  # A shortcut for generating routes namespaced to the Admin module.
+  def path(*args)
+    first, second, rest = args
+
+    if first.is_a?(::ActiveRecord::Base)
+      url_for([:public, *args])
+    elsif first.is_a?(Symbol)
+      if second.is_a?(::ActiveRecord::Base) || second.is_a?(Symbol)
+        url_for([first, :public, second, *rest])
+      else
+        url_for([:public, *args])
+      end
+    end
+  end
 end
