d4b25f37ed022b185404a817b204c164bead2bee
Luke Matthew Sutton <me@lukematthewsutton.com>
2012-10-02 10:34:44 +0930

    Update logs to explicitly type parent_id as integer.

--- config/initializers/logs.rb
+++ config/initializers/logs.rb
@@ -6,7 +6,7 @@ ActivityLog.register(:asset, AssetLogDecorator, %{
     update_status(created_at, updated_at) AS event,
     REPLACE(type, 'Asset', ' - ') || name,
     id,
-    NULL AS parent_id
+    NULL::integer AS parent_id
   FROM assets
   ORDER BY updated_at
 })
@@ -19,7 +19,7 @@ ActivityLog.register(:asset_collection, AssetCollectionLogDecorator, %{
     update_status(created_at, updated_at) AS event,
     name,
     id,
-    NULL AS parent_id
+    NULL::integer AS parent_id
   FROM asset_groups
   WHERE type = 'AssetCollection'
   ORDER BY updated_at
@@ -33,7 +33,7 @@ ActivityLog.register(:asset_group, AssetAlbumLogDecorator, %{
     update_status(created_at, updated_at) AS event,
     name,
     id,
-    NULL AS parent_id
+    NULL::integer AS parent_id
   FROM asset_groups
   WHERE type = 'AssetAlbum'
   ORDER BY updated_at
@@ -47,7 +47,7 @@ ActivityLog.register(:user, UserLogDecorator, %{
     update_status(created_at, updated_at) AS event,
     name,
     id,
-    NULL AS parent_id
+    NULL::integer AS parent_id
   FROM users
   ORDER BY updated_at
 })
