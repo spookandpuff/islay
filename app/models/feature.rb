6f094f0fc6ff5a9fa84fbca61e57d7cad121501a
Ben Hull <ben@spookandpuff.com>
2012-10-19 08:00:12 +1030

    Add links to features

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -8,6 +8,6 @@ class Feature < ActiveRecord::Base
 
   attr_accessible(
     :primary_asset_id, :secondary_asset_id, :title, :description,
-    :styles, :position, :published
+    :styles, :position, :published, :link_url, :link_title
   )
 end
---
+++ db/migrate/20121019075010_alter_features_add_link.rb
@@ -0,0 +1,11 @@
+class AlterFeaturesAddLink < ActiveRecord::Migration
+  def up
+    add_column(:features, :link_url, :string)
+    add_column(:features, :link_title, :string)
+  end
+
+  def down
+    remove_column(:features, :link_url)
+    remove_column(:features, :link_title)
+  end
+end
