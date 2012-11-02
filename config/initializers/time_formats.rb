2fb3d1fb5ebd9a87b0303e6ae8e53fc02feeed03
Ben Hull <ben@spookandpuff.com>
2012-11-02 15:40:22 +1030

    Added a pretty time format, e.g.: Thu, Nov 1 at 12:46am

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,6 @@
 Time::DATE_FORMATS.merge!(
   :default  => "%d %b %Y %H:%M:%S %Z",
-  :param    => "%Y-%m-%d-%H-%M"
+  :param    => "%Y-%m-%d-%H-%M",
+  :pretty   => lambda { |time| time.strftime("%a, %b %e at %l:%M") + time.strftime("%p").downcase }
+
 )
