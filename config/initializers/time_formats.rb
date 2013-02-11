b9448917ddd858a5df4bc0b89beb42152ed6dbd5
Luke Matthew Sutton <me@lukematthewsutton.com>
2013-02-11 14:30:03 +1030

    Add a new short time format.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,6 +1,6 @@
 Time::DATE_FORMATS.merge!(
-  :default  => "%d %b %Y %H:%M:%S %Z",
-  :param    => "%Y-%m-%d-%H-%M",
-  :pretty   => lambda { |time| time.strftime("%a, %b %e at %l:%M") + time.strftime("%p").downcase }
-
+  :default    => "%d %b %Y %H:%M:%S %Z",
+  :param      => "%Y-%m-%d-%H-%M",
+  :pretty     => lambda { |time| time.strftime("%a, %b %e at %l:%M") + time.strftime("%p").downcase },
+  :date_only  => "%d/%m/%Y"
 )
