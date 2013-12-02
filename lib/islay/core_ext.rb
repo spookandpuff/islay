47d4509e05873e2837b551086e8ca39aabf0c3c7
Luke Matthew Sutton <me@lukematthewsutton.com>
2013-12-03 09:23:50 +1030

    Extend the Time class to help traverse time ranges.

--- lib/islay/core_ext.rb
+++ lib/islay/core_ext.rb
@@ -21,3 +21,58 @@ class String
     Hash[ token_pairs ]
   end
 end
+
+# We're reopening the built-in time class and adding some useful methods. These
+# are mainly useful in reporting, but are also useful for querying large 
+# collections of records.
+#
+# They depend on the ActiveSupport Time extensions and thus are not very 
+# portable.
+class Time
+  # Returns the week of the year.
+  #
+  # @return Integer
+  def week
+    strftime("%U").to_i # %W for weeks starting monday
+  end
+
+  # Creates a new instance of time at the specified month, year and time zone.
+  #
+  # @param Integer month
+  # @param Integer year
+  # @param String zone
+  # @return Time
+  def month_in_zone(month, year, zone)
+    zoned = in_time_zone(zone)
+    advance(:years => year - zoned.year, :months => month - zoned.month)
+  end
+
+  # Creates a new instance of time at the specified week of the year, year and
+  # time zone. Care should be taken with this method, since the week of the 
+  # year is dependent on the start of the week, which varies across locales.
+  #
+  # In other words, make sure you're asking for the right week for the zone
+  # specified.
+  #
+  # @param Integer month
+  # @param Integer year
+  # @param String zone
+  # @return Time
+  def week_in_zone(week, year, zone)
+    zoned = in_time_zone(zone)
+    zoned.advance(:years => year - zoned.year, :weeks => week - zoned.week)
+  end
+
+  # Creates an instance of time at the specified day of month, month, year and
+  # time zone.
+  #
+  # @param Integer day
+  # @param Integer month
+  # @param Integer year
+  # @param String zone
+  # @return Time
+  def day_in_zone(day, month, year, zone)
+    zoned = in_time_zone(zone)
+    advance(:years => year - zoned.year, :months => month - zoned.month, :days => day - zoned.day)
+  end
+end
