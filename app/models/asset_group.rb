a4840554a74ce404daa6ba5d395282489799d261
Luke Matthew Sutton <me@lukematthewsutton.com>
2015-02-24 10:30:38 +1030

    Replace Hierarchy dep with a custom concern.

--- app/models/asset_group.rb
+++ app/models/asset_group.rb
@@ -1,34 +1,21 @@
 class AssetGroup < ActiveRecord::Base
-  # include Hierarchy
+  include HierarchyConcern
 
   has_many :assets, -> {order("name")}, :foreign_key => 'asset_group_id'
   class_attribute :kind
   track_user_edits
 
-  # Returns all the groups that are at the top i.e. have no parent.
-  #
-  # @return ActiveRecord::Relation
-  def self.top_level
-    where("path IS NULL or path = ?", '')
-  end
-
-  # @todo Actually implement this
-  def children
-    []
-  end
-
   # Returns the ID of the parent if there is one.
   #
-  # @return [ProductCategory, nil]
+  # @return [AssetGroup, nil]
   def asset_group_id
-    parent.id if parent
+    parent.try(:id)
   end
 
   # Sets the parent via it's ID. If ID is #blank? it does nothing.
   #
   # @param [Integer, String] id
-  #
-  # @return [ProductCategory, nil]
+  # @return [AssetGroup, nil]
   def asset_group_id=(id)
     self.parent = AssetGroup.find(id) unless id.blank?
   end
---
+++ app/models/concerns/hierarchy_concern.rb
@@ -0,0 +1,70 @@
+# A concern that allows models with a "path" attribute of PostgreSQL's LTREE
+# type to be arranged into a hierarchy.
+module HierarchyConcern
+  extend ActiveSupport::Concern
+
+  # Returns the immediate decendents of the record.
+  #
+  # @return ActiveRecord::Relation
+  def children
+    self.class.where("path ~ ?", child_path)
+  end
+
+  # Returns all the decendents of a record, not just it's immediate children.
+  #
+  # @return ActiveRecord::Relation
+  def decendents
+    self.class.where("path ~ ?", "#{child_path}.*")
+  end
+
+  # Returns the ancestors to this record.
+  #
+  # @return ActiveRecord::Relation
+  def ancestors
+    if path
+      self.class.where("id IN (?)", indexed_path.first)
+    else
+      self.class.none
+    end
+  end
+
+  # Returns the parent record - if any.
+  #
+  # @return [ActiveRecord::Base, nil]
+  def parent
+    self.class.find(indexed_path.last) if path
+  end
+
+  # A setter for the parent. This actually just updates the path on the record.
+  #
+  # @param ActiveRecord::Base parent
+  # @return ActiveRecord::Base
+  def parent=(parent)
+    raise ArgumentError, "Parent cannot be a new record" if parent.new_record?
+    self.path = parent.child_path
+    parent
+  end
+
+  # The path to a record's children.
+  #
+  # @return String
+  def child_path
+    path ? "#{path}.#{id}" : id.to_s
+  end
+
+  # Turns the path into an array.
+  #
+  # @return Array<Integer>
+  def indexed_path
+    path ? path.split('.').map(&:to_i) : []
+  end
+
+  module ClassMethods
+    # Returns all the records that are at the top i.e. have no parent.
+    #
+    # @return ActiveRecord::Relation
+    def top_level
+      where("path IS NULL")
+    end
+  end
+end
