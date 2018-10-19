cd818948f74f0698925a30e48246d8abd795d2f1
Ben <ben@spookandpuff.com>
2018-10-19 16:34:40 +1030

    Catch dud logs

--- app/cells/activity_cell.rb
+++ app/cells/activity_cell.rb
@@ -1,7 +1,7 @@
 class ActivityCell < Islay::ApplicationCell
   def log
-    @user_logs = UserActionLog.recent(10).for_user(current_user)
-    @global_logs = ActivityLog.recent(10)
+    @user_logs = UserActionLog.recent(10).for_user(current_user).reject{|l| l.target.blank?}
+    @global_logs = ActivityLog.recent(10).reject{|l| l.model.blank?}
     render
   end
 end
