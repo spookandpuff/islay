cc7d5ad8bdecf1481c78cac0a27be99a59143970
Ben <ben@companionstudio.com.au>
2019-04-05 08:50:06 +1030

    Tweak the namespacing of the asset library controller

--- app/controllers/islay/admin/asset_library_controller.rb
+++ app/controllers/islay/admin/asset_library_controller.rb
@@ -1,26 +1,22 @@
-module Islay
-  module Admin
-    class AssetLibraryController < ApplicationController
-      header 'Asset Library'
-      nav_scope :asset_library
+class Islay::Admin::AssetsController < Islay::Admin::ApplicationController
+  header 'Asset Library'
+  nav_scope :asset_library
 
-      def index
-        @groups         = AssetGroup.summary.order('name')
-        @latest_assets  = Asset.limit(11).order("updated_at DESC")
-        @asset_tags     = AssetTag.order('name')
-      end
+  def index
+    @groups         = AssetGroup.summary.order('name')
+    @latest_assets  = Asset.limit(11).order("updated_at DESC")
+    @asset_tags     = AssetTag.order('name')
+  end
 
-      def browser
-        @albums = AssetGroup.of(params[:only]).order('name ASC')
+  def browser
+    @albums = AssetGroup.of(params[:only]).order('name ASC')
 
-        @assets = if params[:only]
-          Asset.summaries.of(params[:only])
-        else
-          Asset.latest
-        end
-
-        render :layout => false
-      end
+    @assets = if params[:only]
+      Asset.summaries.of(params[:only])
+    else
+      Asset.latest
     end
+
+    render :layout => false
   end
 end
